2f01413ecd0e04d43591ab034e8b5e61d6819612
christopher.yamba <christopher.yamba@dn.se>
2014-11-23 21:47:12 +0100

    fixed SumSquareDifference

---
+++ src/main/java/com/yambacode/solutions/euler1/GroovyEuler1.groovy
@@ -0,0 +1,6 @@
+class GroovyEuler1{
+
+    public static void main(String[] args) {
+        println "hej"
+    }
+}
--- src/main/java/com/yambacode/solutions/euler6/SumSquareDifference.java
+++ src/main/java/com/yambacode/solutions/euler6/SumSquareDifference.java
@@ -1,10 +1,10 @@
 package com.yambacode.solutions.euler6;
 
 
+import com.yambacode.math.Sums;
 import com.yambacode.solutions.AbstractEulerSolver;
 
-import static com.yambacode.math.MathGeneral.squareOfArithmeticSum;
-import static com.yambacode.math.MathGeneral.sumOfSquares;
+
 
 /**
  * Created by cbyamba on 2014-01-13.
@@ -13,6 +13,6 @@ public class SumSquareDifference extends AbstractEulerSolver {
 
     @Override
     public String doSolve() {
-        return "" + (squareOfArithmeticSum(100) - sumOfSquares(100));
+        return "" + (Sums.getSquareOfSums(100) - Sums.getSumOfSquares(100));
     }
 }
