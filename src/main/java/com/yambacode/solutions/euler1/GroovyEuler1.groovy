df883a321081362598b829067d336c246228e242
christopher.yamba <christopher.yamba@dn.se>
2014-11-24 09:38:44 +0100

    Groovy version of euler1

--- src/main/java/com/yambacode/solutions/euler1/GroovyEuler1.groovy
+++ src/main/java/com/yambacode/solutions/euler1/GroovyEuler1.groovy
@@ -1,6 +1,14 @@
-class GroovyEuler1{
+import com.yambacode.solutions.AbstractEulerSolver
+
+class GroovyEuler1 extends AbstractEulerSolver {
+
+
+    @Override
+    String doSolve() {
+        return (1..1000).findAll { it % 3 == 0 || it % 5 == 0 }.sum()
+    }
 
     public static void main(String[] args) {
-        println "test"
+        new GroovyEuler1().solve()
     }
 }
